cf64a028bbc8d56b5d0ec722283380a38a68a898
jibanezm1 <juan.ibanez@outlook.com>
2022-10-11 09:38:05 -0300

    Update db.php

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=papajhons',
+    'dsn' => 'mysql:host=localhost;dbname=$destdir',
     'username' => 'root',
-    'password' => 'root',
+    'password' => 'Casa123___123',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
